a69beb093b157b84d4c0984864aefb28d16dc4a3
Bombaleo <bombaleo97@gmail.com>
2017-07-23 06:27:07 +0300

    Correct stylistic errors

--- app.rb
+++ app.rb
@@ -13,42 +13,46 @@ Dotenv.load
 CLIENT_ID = ENV['GH_BASIC_CLIENT_ID']
 CLIENT_SECRET = ENV['GH_BASIC_SECRET_ID']
 
-use Rack::Session::Pool, :cookie_only => false
+SIGN_UP_ERROR = 'User with this name olready exist.'.freeze
+SIGN_IN_ERROR = 'This user does not exist, please authorizate.'.freeze
+
+use Rack::Session::Pool, cookie_only: false
 
 def authenticated?
   session[:username]
 end
 
 get '/' do
-	if authenticated?
-  	redirect '/submit'
+  if authenticated?
+    redirect '/submit'
   else
-  	erb :auth, :locals => {:client_id => CLIENT_ID}
+    erb :auth, locals: { client_id: CLIENT_ID }
   end
 end
 
 get '/callback' do
   session_code = request.env['rack.request.query_hash']['code']
-  access_token = session[:access_token]
   result = RestClient.post('https://github.com/login/oauth/access_token',
-                        {:client_id => CLIENT_ID,
-                         :client_secret => CLIENT_SECRET,
-                         :code => session_code},
-                         :accept => :json)
+                        { client_id: CLIENT_ID,
+                         client_secret: CLIENT_SECRET,
+                         code: session_code },
+                         accept: :json)
   session[:access_token] = JSON.parse(result)['access_token']
   access_token = session[:access_token]
   begin
     auth_result = RestClient.get('https://api.github.com/user',
-                                 {:params => {:access_token => access_token},
-                                  :accept => :json})
+                                 { params: { access_token: access_token },
+                                  accept: :json })
   rescue => e
+    puts e
     session[:access_token] = nil
   end
   auth_result = JSON.parse(auth_result)
-  @user = User.find_or_create_by(name: auth_result['login'], password: auth_result['login'] )
-  session[:username] = auth_result['login']
+  login = auth_result['login']
+  @user = User.find_or_create_by(name: login, password: login)
+  session[:username] = login
   @user.save
-  redirect "/"
+  redirect '/'
 end
 
 get '/submit' do
@@ -58,14 +62,13 @@ end
 
 post '/submit' do
   @user = User.find_by(params[:user])
-  puts "#{@user}"
-  return (erb :error, :locals => {:message => 'This user does not exist, please authorizate'}) unless @user
-	if @user.save
-		session[:username] = @user.name
+  return (erb :error, locals: { message: SIGN_IN_ERROR }) unless @user
+  if @user.save
+    session[:username] = @user.name
     erb :index
-	else
-		'Sorry, there was an error!'
-	end
+  else
+    'Sorry, there was an error!'
+  end
 end
 
 post '/delurl' do
@@ -76,7 +79,6 @@ end
 
 post '/addurl' do
   @user = User.find_by(name: params[:name])
-  puts @user.name
   @user.urls.create(params[:url])
   erb :index
 end
@@ -86,18 +88,18 @@ get '/signup' do
 end
 
 post '/signup' do
-  @user = User.find_by(name: params[:user]["name"])
-  return erb :error, :locals => {:message => 'User with this name olready exist.'} if @user
+  @user = User.find_by(name: params[:user]['name'])
+  return erb :error, locals: { message: SIGN_UP_ERROR } if @user
   @user = User.create(params[:user])
   if @user.save
-		access_token = session[:access_token]
+    session[:username] = @user.name
     erb :index
-	else
-		'Sorry, there was an error!'
-	end
+  else
+    'Sorry, there was an error!'
+  end
 end
 
 get '/logout' do
-	session[:username] = nil
-	redirect '/'
+  session[:username] = nil
+  redirect '/'
 end
